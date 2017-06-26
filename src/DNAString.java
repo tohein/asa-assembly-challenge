2b3f645acf5d6fc3cfde0fe63631bf0117cfb147
tohein <tohein@stud.uni-saarland.de>
2017-06-27 01:12:53 +0200

    new version of de bruijn graph based on DNAStrings

---
+++ src/DNAString.java
@@ -0,0 +1,245 @@
+import java.util.Arrays;
+
+/**
+ * Immutable class for DNA sequences.
+ *
+ * @author tohei
+ */
+public final class DNAString implements CharSequence {
+
+    /**
+     * Valid characters.
+     */
+    private static final byte[] ALPHABET = {'A', 'C', 'G', 'T'};
+
+    /**
+     * Complement bases.
+     */
+    private static final byte[] COMPLEMENT = new byte[128];
+
+    static {
+        for (int i = 0; i < COMPLEMENT.length; i++) {
+            COMPLEMENT[i] = (byte) i;
+        }
+        COMPLEMENT['a'] = COMPLEMENT['A'] = 'T';
+        COMPLEMENT['c'] = COMPLEMENT['C'] = 'G';
+        COMPLEMENT['g'] = COMPLEMENT['G'] = 'C';
+        COMPLEMENT['t'] = COMPLEMENT['T'] = 'A';
+    }
+
+    /**
+     * Hash value.
+     */
+    private int hash;
+
+    /**
+     * DNA sequence.
+     */
+    private final byte[] sequence;
+
+    /**
+     * Construct DNAString from byte array.
+     *
+     * @param sequence byte array.
+     */
+    public DNAString(byte[] sequence) {
+        this.sequence = sequence;
+    }
+
+    /**
+     * Construct DNAString from String.
+     *
+     * @param s String sequence.
+     */
+    public DNAString(String s) {
+        this.sequence = s.getBytes();
+    }
+
+    /**
+     * Construct empty DNAString.
+     */
+    public DNAString() {
+        this.sequence = new byte[0];
+    }
+
+    /**
+     * Compute reverse complement of DNA sequence.
+     *
+     * @return DNAString of reverse complement.
+     */
+    public DNAString reverseComplement() {
+        byte[] rcSequence = new byte[length()];
+        for (int i = 0; i < length(); i++) {
+            rcSequence[length() - i - 1] = COMPLEMENT[sequence[i]];
+        }
+        return new DNAString(rcSequence);
+    }
+
+    /**
+     * Compute all variations at given position. If pos is outside of sequence array, return extensions.
+     *
+     * @param pos position index.
+     * @return DNAString array containing variations of sequence at position pos.
+     */
+    public DNAString[] allVariations(int pos) {
+        DNAString[] var = new DNAString[ALPHABET.length];
+        int len = length();
+        if (pos < 0) {
+            pos = 0;
+            len = len + 1;
+        } else if (pos >= len) {
+            pos = len;
+            len = len + 1;
+        }
+        for (int i = 0; i < ALPHABET.length; i++) {
+            byte[] newSeq = new byte[len];
+            for (int j = 0; j < pos; j++) {
+                newSeq[j] = sequence[j];
+            }
+            int shift = len - length();
+            for (int j = pos + 1; j < len; j++) {
+                newSeq[j] = sequence[j - shift];
+            }
+            newSeq[pos] = ALPHABET[i];
+            var[i] = new DNAString(newSeq);
+        }
+        return var;
+    }
+
+    /**
+     * Concatenates the specified string to the end of this string.
+     * <p>
+     * If the length of the argument string is 0, then this DNAString object is returned. Otherwise, a new
+     * DNAString object is created, representing a byte sequence that is the concatenation of the byte sequence
+     * represented by this DNAString object and the byte sequence represented by the argument DNAString.
+     *
+     * @param s the DNAString that will will be concatenated to the end of this DNString.
+     * @return a new DNAString object representing the sequence of this DNAString followed by the sequence of the
+     * argument's sequence.
+     */
+    public DNAString concat(DNAString s) {
+        if (s == null) {
+            throw new NullPointerException();
+        }
+        if (s.length() == 0) {
+            return this;
+        } else {
+            int len1 = this.length();
+            int len2 = s.length();
+            byte[] concatenation = new byte[len1 + len2];
+            for (int i = 0; i < len1; i++) {
+                concatenation[i] = this.sequence[i];
+            }
+            for (int i = len1; i < concatenation.length; i++) {
+                concatenation[i] = s.sequence[i - len1];
+            }
+            return new DNAString(concatenation);
+        }
+    }
+
+    /**
+     * Returns the byte representation of the base at position pos.
+     *
+     * @param pos position to evaluate.
+     * @return char representation of byte at position pos.
+     */
+    public byte byteAt(int pos) {
+        return sequence[pos];
+    }
+
+    /**
+     * Returns the underlying byte sequence.
+     *
+     * @return byte sequence of this DNAString.
+     */
+    public byte[] toByteArray() {
+        return sequence.clone();
+    }
+
+    /**
+     * Compare this DNAString to the given object. Two DNAStrings are equal if their byte sequences are the same.
+     *
+     * @param o an Object to compare to this DNAString.
+     * @return true if the object is a DNAString object with the same byte sequence.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || !(o instanceof DNAString)) {
+            return false;
+        } else if (o == this) {
+            return true;
+        } else {
+            DNAString dnaString = (DNAString) o;
+            return Arrays.equals(this.sequence, dnaString.sequence);
+        }
+    }
+
+    /**
+     * Returns a hash code for this string. The hash code for a DNAString object is computed in the same way
+     * as for a regular String object:
+     * <p>
+     * s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
+     *
+     * @return a hash code for this DNAString.
+     */
+    @Override
+    public int hashCode() {
+        int h = hash;
+        if (h == 0 && length() > 0) {
+            for (int i = 0; i < length(); i++) {
+                h = 31 * h + sequence[i];
+            }
+            hash = h;
+        }
+        return h;
+    }
+
+    /**
+     * Returns length of this DNAString
+     *
+     * @return length of the byte array containing the bases of this DNAString.
+     */
+    @Override
+    public int length() {
+        return sequence.length;
+    }
+
+    /**
+     * Returns the char representation of the base at position i.
+     *
+     * @param i position to evaluate.
+     * @return char representation of byte at position i.
+     */
+    @Override
+    public char charAt(int i) {
+        return (char) sequence[i];
+    }
+
+    /**
+     * Returns a new DNAString representing a subsequence of this DNAString.
+     *
+     * @param from the begin index, inclusive.
+     * @param to   the end index, exclusive.
+     * @return the specified subsequence.
+     */
+    @Override
+    public DNAString subSequence(int from, int to) {
+        return new DNAString(Arrays.copyOfRange(sequence, from, to));
+    }
+
+    /**
+     * Turn this DNAString into a regular String.
+     *
+     * @return a String representation of this DNAString
+     */
+    @Override
+    public String toString() {
+        return new String(sequence);
+    }
+
+    public static void main(String[] args) {
+        DNAString dna = new DNAString("ACGT");
+        String s = "hello";
+        System.out.println(dna.subSequence(4, 4));
+    }
+}
---
+++ src/ReadCorrector.java
@@ -0,0 +1,408 @@
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Class for error correction of DNAString reads.
+ * <p>
+ * All error correction methods are variations of spectral alignment.
+ * <p>
+ * (1) Majority Alignment Correction (MAC) - correction by majority vote
+ * (2) Consensus Alignment Correction (CAC) - like MAC, but relying on unanimous consensus
+ * (3) Spectral Alignment Correction (SAC) - basic spectral alignment
+ * <p>
+ * Created by tohei on 6/26/17.
+ */
+public class ReadCorrector {
+
+    /**
+     * k-mer length.
+     */
+    private int k;
+
+    /**
+     * Coverage cutoff to distinguish between solid (coverage above cutoff) and weak (coverage below cutoff) k-mers.
+     */
+    private int cutoff;
+
+    /**
+     * Reads to correct.
+     */
+    private DNAString[] reads;
+
+    /**
+     * Coverage counts of all k-mers in reads.
+     */
+    private LinkedHashMap<DNAString, Integer> kmerCounts;
+
+    /**
+     * Get counts of k-mers and their reverse complements.
+     *
+     * @return LinkedHashSet mapping k-mers to counts.
+     */
+    public Map<DNAString, Integer> getCounts() {
+        return Collections.unmodifiableMap(kmerCounts);
+    }
+
+    /**
+     * Associate this ReadCorrector with the given reads.
+     *
+     * @param reads reads to work on.
+     * @param k     k-mer length.
+     */
+    public void setReads(DNAString[] reads, int k) {
+        this.k = k;
+        this.reads = reads;
+        kmerCounts = DNAStringUtils.kmerCounts(reads, k);
+    }
+
+    /**
+     * Setter for coverage cutoff.
+     *
+     * @param cutoff integer coverage cutoff.
+     */
+    public void setCutoff(int cutoff) {
+        this.cutoff = cutoff;
+    }
+
+    /**
+     * Convenience class for saving replacement votes of k-mers in a read.
+     */
+    private class ReplacementList {
+        /**
+         * Array of maps relating bases to votes.
+         */
+        private LinkedHashMap<Byte, Integer>[] candidateReplacements;
+
+        /**
+         * Old read.
+         */
+        private DNAString old;
+
+        /**
+         * Maximum number of replacements per read (majority vote).
+         */
+        private final int MAX_REPLACEMENTS = 4;
+
+        /**
+         * Create new ReplacementList.
+         *
+         * @param old read to correct.
+         */
+        public ReplacementList(DNAString old) {
+            this.old = old;
+            this.candidateReplacements = new LinkedHashMap[old.length()];
+            for (int i = 0; i < old.length(); i++) {
+                this.candidateReplacements[i] = new LinkedHashMap<Byte, Integer>();
+            }
+        }
+
+        /**
+         * Get the length of the old read.
+         *
+         * @return
+         */
+        public int getListLength() {
+            return old.length();
+        }
+
+        /**
+         * Add a new candidate to the list (if does not exist) or update the count of an existing
+         * candidate (if it does).
+         *
+         * @param pos position to update.
+         * @param c   base voted for.
+         */
+        public void addCandidate(int pos, byte c) {
+            addCandidate(pos, c, 1);
+        }
+
+        /**
+         * Add a new candidate to the list (if does not exist) or update the count of an existing
+         * candidate (if it does).
+         *
+         * @param pos    position to update.
+         * @param c      base voted for.
+         * @param weight weight of the vote.
+         */
+        public void addCandidate(int pos, byte c, int weight) {
+            Integer count = candidateReplacements[pos].get(c);
+            if (count == null) {
+                candidateReplacements[pos].put(c, weight);
+            } else {
+                candidateReplacements[pos].put(c, count + weight);
+            }
+        }
+
+        /**
+         * Compute the consensus replacement of all bases in the read.
+         *
+         * @return corrected read (DNAString).
+         */
+        public DNAString getConsensusReplacement() {
+            byte[] seq = new byte[getListLength()];
+            for (int i = 0; i < getListLength(); i++) {
+                seq[i] = getConsensusCandidate(i);
+            }
+            return new DNAString(seq);
+        }
+
+        /**
+         * The consensus replacement at pos is the majority candidate (if the vote is unanimous) or
+         * the old base (if it is not)
+         *
+         * @param pos position to evaluate.
+         * @return consensus base.
+         */
+        private byte getConsensusCandidate(int pos) {
+            LinkedHashMap<Byte, Integer> posCandidates = candidateReplacements[pos];
+
+            if (posCandidates.keySet().size() == 1) {
+                return posCandidates.keySet().iterator().next();
+            } else {
+                return this.old.byteAt(pos);
+            }
+        }
+
+        /**
+         * Compute the majority replacement of all bases in the read.
+         *
+         * @return corrected read (DNAString).
+         */
+        public DNAString getMajorityReplacement() {
+            int[] topCandidatePos = new int[MAX_REPLACEMENTS];
+            // default initialization is all zeros
+            int[] topCandidateScores = new int[MAX_REPLACEMENTS];
+
+            for (int i = 0; i < getListLength(); i++) {
+                byte c = getMajorityCandidate(i);
+                int score = candidateReplacements[i].get(c);
+                for (int j = 0; j < topCandidatePos.length; j++) {
+                    if (score > topCandidateScores[j]) {
+                        topCandidatePos[j] = i;
+                        topCandidateScores[j] = score;
+                        break;
+                    }
+                }
+            }
+            byte[] repl = old.toByteArray();
+            for (int i = 0; i < topCandidatePos.length; i++) {
+                int pos = topCandidatePos[i];
+                repl[pos] = getMajorityCandidate(pos);
+            }
+            return new DNAString(repl);
+        }
+
+        /**
+         * Find the majority candidate at a given position.
+         *
+         * @param pos position to evaluate.
+         * @return majority base.
+         */
+        private byte getMajorityCandidate(int pos) {
+            LinkedHashMap<Byte, Integer> posCandidates = candidateReplacements[pos];
+            int maxCount = -1;
+            byte topRepl = 0;
+            for (Byte c : posCandidates.keySet()) {
+                if (posCandidates.get(c) > maxCount) {
+                    topRepl = c;
+                }
+            }
+            return topRepl;
+        }
+
+        /**
+         * Construct a string representation of the votes (for debugging purposes).
+         *
+         * @return a String representation of this ReplacementList.
+         */
+        @Override
+        public String toString() {
+            String s = "  ";
+            for (int i = 0; i < getListLength(); i++) {
+                s = s + i + " ";
+            }
+            final char[] alphabet = {'A', 'C', 'G', 'T'};
+            for (char c : alphabet) {
+                s = s + "\n";
+                s = s + c + " ";
+                for (int i = 0; i < getListLength(); i++) {
+                    Integer count = candidateReplacements[i].get(c);
+                    if (count == null) {
+                        s = s + "0 ";
+                    } else {
+                        s = s + count.toString() + " ";
+                    }
+                }
+            }
+            return s;
+        }
+    }
+
+    /**
+     * (M)ajority (A)lignment (C)orrection.
+     * <p>
+     * Every k-mer in a read 'votes' for a replacement of itself by its best Hamming neighbor. The vote is weighted by
+     * the coverage of that k-mer. Every base is replaced by the majority candidate of the respective best Hamming
+     * neighbors of all overlapping k-mers.
+     * k-mers.
+     *
+     * @param verbose be verbose.
+     * @return the number of corrected reads.
+     */
+    public int computeMAC(boolean verbose) {
+        long startTime = System.currentTimeMillis();
+        if (verbose) System.out.print("Computing majority alignment (cutoff = " + cutoff + ") ... ");
+        int numOfRepl = 0;
+
+        for (int i = 0; i < reads.length; i++) {
+            DNAString read = reads[i];
+            ReplacementList cand = new ReplacementList(read);
+            for (int j = 0; j < read.length() - k + 1; j++) {
+                DNAString v = read.subSequence(j, j + k);
+                if (kmerCounts.get(v) < cutoff) {
+                    v = bestHammingNeighbor(v);
+                }
+                int weight = kmerCounts.get(v);
+                for (int l = 0; l < k; l++) {
+                    cand.addCandidate(j + l, v.byteAt(l), weight);
+                }
+            }
+            reads[i] = cand.getMajorityReplacement();
+            if (verbose) {
+                /*System.out.println("\n" + read + ": ");
+                System.out.println(cand.toString());*/
+                if (!read.equals(reads[i])) {
+                    numOfRepl++;
+                }
+            }
+        }
+        if (verbose) {
+            long endTime = System.currentTimeMillis();
+            System.out.println("done (" + (endTime - startTime) / 1000 + " seconds).");
+            System.out.println(numOfRepl + " of " + reads.length + " reads were corrected.");
+        }
+        kmerCounts = DNAStringUtils.kmerCounts(reads, k);
+        return numOfRepl;
+    }
+
+    /**
+     * (C)onsensus (A)lignment (C)orrection.
+     * <p>
+     * Every k-mer in a read 'votes' for a replacement of itself by its best Hamming neighbor. A base is replaced, if
+     * all overlapping k-mers agree on that base.
+     * k-mers.
+     *
+     * @param verbose be verbose.
+     * @return the number of corrected reads.
+     */
+    public int computeCAC(boolean verbose) {
+        long startTime = System.currentTimeMillis();
+        if (verbose) System.out.print("Computing majority alignment (cutoff = " + cutoff + ") ... ");
+        int numOfRepl = 0;
+
+        for (int i = 0; i < reads.length; i++) {
+            DNAString read = reads[i];
+            ReplacementList cand = new ReplacementList(read);
+            for (int j = 0; j < read.length() - k + 1; j++) {
+                DNAString v = read.subSequence(j, j + k);
+                if (kmerCounts.get(v) < cutoff) {
+                    v = bestHammingNeighbor(v);
+                }
+                int weight = kmerCounts.get(v);
+                for (int l = 0; l < k; l++) {
+                    cand.addCandidate(j + l, v.byteAt(l));
+                }
+            }
+            reads[i] = cand.getConsensusReplacement();
+            if (verbose) {
+                /*System.out.println("\n" + read + ": ");
+                System.out.println(cand.toString());*/
+                if (!read.equals(reads[i])) {
+                    numOfRepl++;
+                }
+            }
+        }
+        if (verbose) {
+            long endTime = System.currentTimeMillis();
+            System.out.println("done (" + (endTime - startTime) / 1000 + " seconds).");
+            System.out.println(numOfRepl + " of " + reads.length + " reads were corrected.");
+        }
+        kmerCounts = DNAStringUtils.kmerCounts(reads, k);
+        return numOfRepl;
+    }
+
+    /**
+     * (S)ectral (A)lignment (C)orrection.
+     * <p>
+     * Low coverage kmers will be replaced by their best Hamming neighbor.
+     *
+     * @param verbose be verbose.
+     * @return number of replacements made.
+     */
+    public int computeSAC(boolean verbose) {
+        long startTime = System.currentTimeMillis();
+        if (verbose) System.out.print("Computing spectral alignment (cutoff = " + cutoff + ") ... ");
+
+        int numOfRepl = 0;
+        for (int i = 0; i < reads.length; i++) {
+            DNAString read = reads[i];
+            for (int j = 0; j < read.length() - k + 1; j++) {
+                DNAString v = read.subSequence(j, j + k);
+                if (kmerCounts.get(v) < cutoff) {
+                    numOfRepl++;
+                    reads[i] = read.subSequence(0, j).concat(bestHammingNeighbor(v)).concat(read.subSequence(j + k, read.length()));
+                }
+            }
+        }
+        if (verbose) {
+            long endTime = System.currentTimeMillis();
+            System.out.println("done (" + (endTime - startTime) / 1000 + " seconds).");
+            System.out.println("(Total of " + numOfRepl + " replacements)");
+        }
+        kmerCounts = DNAStringUtils.kmerCounts(reads, k);
+        return numOfRepl;
+    }
+
+    /**
+     * Find best Hamming neighbor of v.
+     * <p>
+     * Searches for the solid k-mer with the highest count which only differs in one base from v. If no such
+     * k-mer exists, v will be returned.
+     *
+     * @param seq input sequence.
+     * @return best Hamming neighbor of v.
+     */
+    private DNAString bestHammingNeighbor(DNAString seq) {
+        DNAString bestSeq = seq;
+        int bestCount = cutoff - 1;
+        for (int i = 0; i < seq.length(); i++) {
+            for (DNAString candidate : seq.allVariations(i)) {
+                if (kmerCounts.containsKey(candidate) && (kmerCounts.get(candidate) > bestCount)) {
+                    bestSeq = candidate;
+                    bestCount = kmerCounts.get(candidate);
+                }
+            }
+        }
+        return bestSeq;
+    }
+
+    public static void main(String[] args) {
+        String[] reads = {"ACTA", "TAGT", "ACTA", "ACTT"};
+        DNAString[] reads2 = new DNAString[reads.length];
+        for (int i = 0; i < reads.length; i++) {
+            reads2[i] = new DNAString(reads[i]);
+        }
+        System.out.println("Old reads:");
+        for (DNAString read : reads2) {
+            System.out.print(read.toString() + " ");
+        }
+        ReadCorrector r = new ReadCorrector();
+        r.setReads(reads2, 3);
+        r.setCutoff(2);
+        r.computeSAC(true);
+        for (DNAString read : reads2) {
+            System.out.print(read.toString() + " ");
+        }
+    }
+}
